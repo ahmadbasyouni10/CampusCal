249a986b608947463a3ed0578e0c5e9d88aa11b9
ahmadbasyouni10 <ahmadnayef2004@gmail.com>
2024-07-11 02:13:15 -0400

    Setup local storage for auth and routed the components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,59 @@
 import './App.css';
-import React, { useState, useRef } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 import Calendar from './components/Calendar';
-import { DayPilotCalendar, DayPilotNavigator } from "@daypilot/daypilot-lite-react";
+import LoginForm from './components/Login';
+import RegistrationForm from './components/Register';
 import 'react-calendar/dist/Calendar.css';
 import "./CalendarStyles.css";
 
+function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
 
-export default function CalendarGfg() {
-	const [config, setConfig] = useState({
-        viewType: "Week",
-        durationBarVisible: false
-      });
-      
-      const calendarRef = useRef();
-  
-      const handleTimeRangeSelected = args => {
-        calendarRef.current.control.update({
-          startDate: args.day
-        });
-      }  
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      setLoggedIn(true);
+    }
+  }, []);
 
-	return (
-		<div>
-			<h1> Calendar </h1>
-            <Calendar/>
+  const handleLogin = () => {
+    setLoggedIn(true);
+  };
 
-        </div>
-    );
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    setLoggedIn(false);
+  };
+
+  return (
+    <Router>
+      <div className="App">
+        <header>
+          <nav>
+            <ul>
+              {loggedIn ? (
+                <>
+                  <li><Link to="/">Home</Link></li>
+                  <li><Link to="/" onClick={handleLogout} className='logout-button'>Logout</Link></li>
+                </>
+              ) : (
+                <>
+                  <li><Link to="/login">Login</Link></li>
+                  <li><Link to="/register">Register</Link></li>
+                </>
+              )}
+            </ul>
+          </nav>
+        </header>
+        <Routes>
+          <Route path="/" element={loggedIn ? <Calendar /> : <Navigate replace to="/login" />} />
+          <Route path="/login" element={!loggedIn ? <LoginForm onLogin={handleLogin} /> : <Navigate replace to="/" />} />
+          <Route path="/register" element={!loggedIn ? <RegistrationForm onLogin={handleLogin} /> : <Navigate replace to="/" />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-
-//goes inside main fucntion for App.js
-//<button onClick={() => setButtonPopup(true)} color='teal'>Create Event</button>
+export default App;
