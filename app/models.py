e03995077540e0facd2c57eb604582a40a74fb79
ahmadbasyouni10 <ahmadnayef2004@gmail.com>
2024-07-11 02:14:17 -0400

    Fixed User model nullable values

--- app/models.py
+++ app/models.py
@@ -2,17 +2,16 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_login import UserMixin
-from app.db import db
+from app import db
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
-    sleep_hours = db.Column(db.Integer, nullable=False, default=8)
-    other_commitments = db.Column(db.Text, nullable=True)
-    study_hours_per_day = db.Column(db.Integer, nullable=False, default=2)
-    preferred_study_time = db.Column(db.String(20), nullable=False, default="morning")  # 'morning' or 'night'
+    sleep_hours = db.Column(db.Integer, default=8)
+    other_commitments = db.Column(db.Text)
+    study_hours_per_day = db.Column(db.Integer, default=2)
+    preferred_study_time = db.Column(db.String(20), default="morning")  # 'morning' or 'night'
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}')"
