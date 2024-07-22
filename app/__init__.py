25948a639fa4c943057bed4de5a623d42d86678f
ahmadbasyouni10 <ahmadnayef2004@gmail.com>
2024-07-22 14:55:09 -0400

    Deployment serve react app using flask

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS  # Import CORS
 import os
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 db = SQLAlchemy()
 load_dotenv()
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='../frontend/build', static_url_path='')
     app.config['DEBUG'] = True 
 
     # Load environment variables
@@ -28,5 +28,13 @@ def create_app():
 
     with app.app_context():
         db.create_all()
+    
+    @app.route('/', defaults={'path': ''})
+    @app.route('/<path:path>')
+    def serve(path):
+        if path != "" and os.path.exists(app.static_folder + '/' + path):
+            return send_from_directory(app.static_folder, path)
+        else:
+            return send_from_directory(app.static_folder, 'index.html')
 
     return app
