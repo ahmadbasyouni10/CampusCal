fe8608918d0a4ac1b6d3f1ca2e5c83476170927c
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2024-07-23 12:45:43 -0400

    deploy

--- wsgi.py
+++ wsgi.py
@@ -15,4 +15,4 @@ def serve(path):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
