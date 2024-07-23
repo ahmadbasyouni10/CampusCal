aa7897e3cde37a5fa99d195a7107d7980d26dbd4
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2024-07-23 13:59:15 -0400

    deploy

--- app/__init__.py
+++ app/__init__.py
@@ -1,30 +1,34 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS  # Import CORS
 import os
+from flask import Flask, send_from_directory
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 from dotenv import load_dotenv
 
-# Initialize SQLAlchemy
 db = SQLAlchemy()
 load_dotenv()
-def create_app():
-    app = Flask(__name__)
-    app.config['DEBUG'] = True 
 
-    # Load environment variables
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI', 'sqlite:///CampusCal.db')
+def create_app():
+    app = Flask(__name__, static_folder='../frontend/build', static_url_path='')
+    app.config['DEBUG'] = False  # Set to False for production
+
+    # Use the Internal Database URL from Render
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'mysecret')
 
-    # Apply CORS to the entire app
-    CORS(app, resources={r"/*": {"origins": "*"}})  # This allows all origins, adjust as needed
-
-    # Initialize extensions
+    CORS(app)
     db.init_app(app)
 
-    # Import and register blueprints
     from app.routes import bp as login_bp
     app.register_blueprint(login_bp)
+
+    @app.route('/', defaults={'path': ''})
+    @app.route('/<path:path>')
+    def serve(path):
+        if path != "" and os.path.exists(app.static_folder + '/' + path):
+            return send_from_directory(app.static_folder, path)
+        else:
+            return send_from_directory(app.static_folder, 'index.html')
 
     with app.app_context():
         db.create_all()
