4bed3e9e7c91282de1c344f8aa2e429fef32eec4
acabrera04 <acabrera04164@gmail.com>
2024-07-24 10:54:14 -0400

    Updated building for backend

--- start.sh
+++ start.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-apt-get update
-apt-get install -y build-essential cython
-python -m pip install --upgrade pip
-pip install -r requirements.txt
+apt-get update && apt-get install -y build-essential cython
