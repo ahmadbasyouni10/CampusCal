8554f89de62c8886a3d15721e04e330a1af2efce
acabrera04 <acabrera04164@gmail.com>
2024-07-24 10:50:43 -0400

    Fixed startup script

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install -y build-essential cython
+apt-get update
+apt-get install -y build-essential cython
 python -m pip install --upgrade pip
 pip install -r requirements.txt
